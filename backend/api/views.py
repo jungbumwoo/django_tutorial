851ded094cbadf459829fcd85c267163f73027d4
Jungbum Woo <jungbumwoo@naver.com>
2022-04-23 08:22:48 +0900

    initial api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
+    print('over here')
     return JsonResponse({"message": "Hi there, this is your Django API response!!"})
     
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,7 @@
 import requests
 
-endpoint = "https://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint, json={"query": "Hello, world!"})
 
-print(get_response.text)
 print(get_response.json())
