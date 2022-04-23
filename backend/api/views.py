da52341a4a066d1b0f734d0470e683c2e8ba4429
Jungbum Woo <jungbumwoo@naver.com>
2022-04-23 09:51:30 +0900

    model instance

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
+
 def api_home(request, *args, **kwargs):
     body = request.body # byte string of JSON data
     data = {}
@@ -15,4 +17,14 @@ def api_home(request, *args, **kwargs):
     data['params'] = dict(request.GET)
     data['headers'] = dict(request.headers)
     data['content_type'] = request.content_type
+    return JsonResponse(data)
+
+def api_random(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by('?').first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id 
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
