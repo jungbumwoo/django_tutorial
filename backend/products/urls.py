448c949229372e7541f1242e53520ab01aeb78ba
Jungbum Woo <jungbumwoo@naver.com>
2022-04-25 09:14:15 +0900

    ListAPIView & ListCreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view())
+    path('create/', views.ProductCreateAPIView.as_view()),
+    path('create/list/', views.ProductListCreateAPIView.as_view()),
+    path('showlist/', views.ProductListAPIView.as_view()),
+    path('<int:pk>/', views.product_detail_view), # ProductDetailAPIView.as_view()
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,6 @@
+from syslog import closelog
 from rest_framework import generics
+from yaml import serialize
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -19,3 +21,18 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
